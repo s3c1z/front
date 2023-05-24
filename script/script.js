71b556d32e1e44304090b138acee63666ff3ffce
S3C1Z <zabyza2@gmail.com>
2023-05-24 20:47:36 +0300

    added request to swagger server

--- script/script.js
+++ script/script.js
@@ -1,11 +1,11 @@
-let email = document.querySelector('#email');
+let username = document.querySelector('#username');
 let password = document.querySelector('#password');
 let submit = document.querySelector('#submit');
 
 let users = {};
 
-function User(email, password) {
-    this.email = email;
+function User(username, password) {
+    this.username = username;
     this.password = password;
 }
 
@@ -14,13 +14,36 @@ function createId(users) {
 }
 
 submit.addEventListener('click', () => {
-    const emailUser = email.value;
+    const usernameUser = username.value;
     const passwordUser = password.value;
 
-    const user = new User(emailUser, passwordUser);
+    const user = new User(usernameUser, passwordUser);
 
     const userId = 'User' + createId(users);
     users[userId] = user;
 
     console.log(users);
+
+    const requestBody = {
+        username: usernameUser,
+        password: passwordUser
+    };
+
+    fetch('https://26.157.2.248:7272/api/Auth/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(requestBody)
+    })
+    .then(response => {
+        if (response.ok) {
+            console.log('Данные успешно отправлены в Swagger!');
+        } else {
+            console.error('Ошибка при отправке данных в Swagger:', response.status);
+        }
+    })
+    .catch(error => {
+        console.error('Ошибка при отправке данных в Swagger:', error);
+    });
 });
